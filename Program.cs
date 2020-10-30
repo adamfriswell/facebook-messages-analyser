e69a2ef68420f1a75bab00ffecbb40bae1df7d95
Adam Friswell <adamfriswell@googlemail.com>
2020-10-30 19:14:46 +0000

    new dotnet console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace facebook_messages_analyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
